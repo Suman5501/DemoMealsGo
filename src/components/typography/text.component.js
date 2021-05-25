9c470eac29574bc607090c54482e1f98cce4b124
Suman <sumankri5501@gmail.com>
2021-05-25 19:42:28 +0530

    initial commit

---
+++ src/components/typography/text.component.js
@@ -0,0 +1,50 @@
+import React from "react";
+import styled from "styled-components/native";
+
+const defaultTextstyles = (theme) => `
+  font-family: ${theme.fonts.body};
+  font-weight: ${theme.fontWeights.regular};
+  color: ${theme.colors.text.primary};
+  flex-wrap: wrap;
+  margin-top: 0px;
+  margin-bottom: 0px;
+`;
+
+const body = (theme) => `
+  font-size: ${theme.fontsizes.body};
+`;
+
+const hint = (theme) => `
+  font-size: ${theme.fontsizes.body};
+`;
+
+const error = (theme) => `
+  color: ${theme.colors.text.error};
+`;
+
+const caption = (theme) => `
+  font-size: ${theme.fontsizes.caption};
+  font-family: ${theme.fontWeights.bold};
+`;
+
+const label = (theme) => `
+  font-family: ${theme.fonts.heading};
+  font-weight: ${theme.fontWeights.medium};
+  font-size: ${theme.fontsizes.body};
+`;
+const variants = {
+  body,
+  hint,
+  error,
+  label,
+  caption,
+};
+
+export const Text = styled.Text`
+  ${({ theme }) => defaultTextstyles(theme)}
+  ${({ variant, theme }) => variants[variant](theme)}
+`;
+
+Text.defaultProps = {
+  variant: "body",
+};
